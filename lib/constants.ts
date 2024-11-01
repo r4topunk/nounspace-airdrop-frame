e2ef1802931ca271e373de7756411d0093049406
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-11-01 16:25:26 -0300

    refactor: add Neynar API key and client ID to environment variables

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,5 @@ export const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as `0x${string}
 export const WEBSITE_URL = process.env.WEBSITE_URL || "http://localhost:3000"
 export const SUPABASE_URL = process.env.SUPABASE_URL!
 export const SUPABASE_KEY = process.env.SUPABASE_KEY!
+export const NEYNAR_API_KEY= process.env.NEYNAR_API_KEY!
+export const NEYNAR_CLIENT_ID= process.env.NEYNAR_CLIENT_ID!

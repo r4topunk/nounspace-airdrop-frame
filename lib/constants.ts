1ac2a7a1ba45cef5f0bc843a132e5798bed63f8a
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-11-01 18:52:14 -0300

    refactor: update WEBSITE_URL to support Vercel deployment URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const AIRDROP_TOKEN_ADDRESS = process.env.AIRDROP_TOKEN_ADDRESS as `0x${string}`
 export const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as `0x${string}`
-export const WEBSITE_URL = process.env.WEBSITE_URL || "http://localhost:3000"
+export const WEBSITE_URL = process.env.WEBSITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000"
 export const SUPABASE_URL = process.env.SUPABASE_URL!
 export const SUPABASE_KEY = process.env.SUPABASE_KEY!
 export const NEYNAR_API_KEY= process.env.NEYNAR_API_KEY!
